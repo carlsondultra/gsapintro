26abb7b7ff88eb2d12a1fdcd0bc224f78a761ccc
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-17 18:32:04 -0400

    adding commas and semicolons

--- app/components/hero/index.js
+++ app/components/hero/index.js
@@ -3,45 +3,49 @@ import { gsap } from "gsap";
 import Image from "next/image";
 
 import styles from "./Hero.module.scss";
-
 import { animateTitle, animateImage, revealMenu } from "./animations";
 
 const Hero = () => {
-
-  const heroRef = useRef(null);
   const timeline = useRef(gsap.timeline());
+  const heroRef = useRef(null);
 
   useEffect(() => {
     const context = gsap.context(() => {
       const tl = timeline.current;
-      
 
-      tl.add(animateTitle()).add(animateImage(), 0).add(revealMenu(), 0);
-    })
+      tl.add(animateTitle()).add(revealMenu(), 0).add(animateImage(), 0);
+    }, heroRef);
 
-    return () => context.revert()
-  }, heroRef)
+    return () => context.revert();
+  }, []);
 
   return (
     <section className={styles.hero} ref={heroRef}>
       <div className={styles.hero__top}>
         <div data-menu-item data-hidden>
-          <p>Hello!</p>
+          Hello!
         </div>
-        <span data-menu-item data-hidden>about</span>
-        <span data-menu-item data-hidden>contact</span>
+        <span data-menu-item data-hidden>
+          about
+        </span>
+        <span data-menu-item data-hidden>
+          contact
+        </span>
       </div>
 
       <h1 className={styles.hero__title}>
-        <span data-title-first data-hidden>gsap</span>
+        <span data-hidden data-title-first>
+          Ultra
+        </span>
         <span data-hero-line className={styles.hero__line}></span>
-        <span data-title-last data-hidden>intro</span>
+        <span data-hidden data-title-last>
+          agency
+        </span>
       </h1>
 
       <div className={styles.hero__image}>
         <div data-image-overlay className={styles.hero__imageOverlay}></div>
         <Image
-          // data-hidden
           data-image
           src="/images/blob.jpg"
           width={1728}
